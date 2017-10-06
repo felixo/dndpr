15f39af68f9170e042f3d704891961aebf12560a
Felix <alexei.eremin@gmail.com>
2017-10-06 22:14:48 +0300

    working on carusel

--- presscorp/models.py
+++ presscorp/models.py
@@ -1,6 +1,15 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
+class Slider(models.Model):
+    number = models.IntegerField(default=0)
+    img = models.FileField(upload_to='media/slider')
+    link = models.CharField(max_length=100, default='/')
+    name = models.CharField(max_length=50, default='Слайдер №')
+
+    def __unicode__(self):  # __unicode__ on Python 2
+        return unicode(self.name) or u''
 # Create your models here.
---
+++ presscorp/static/presscorp/script.js
@@ -0,0 +1,5 @@
+ $(document).ready(function() {
+    $('.nav-toggle').on('click', function(){
+        $('#menu').toggleClass('active');
+    });
+ });
